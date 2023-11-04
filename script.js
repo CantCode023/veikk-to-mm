3e20e3ae3b6dc5ec3266f99b4053999adcf26910
cantcode023 <cantcode023@gmail.com>
2023-11-04 16:42:43 +0800

    updated formula logic to scale bottom and right alignment position with widht and height

--- script.js
+++ script.js
@@ -6,7 +6,13 @@ calculatorForm.addEventListener("submit", (e) => {
     let bottom = document.getElementById("bottom");
     let right = document.getElementById("right");
     let left = document.getElementById("left");
+
+    let width = (parseFloat(right.value)-parseFloat(left.value))/200;
+    let height = (parseFloat(bottom.value)-parseFloat(top.value))/200;
+    let X = 76.2 + ((152.4 - width)/2);
+    let Y = 50.8 + ((101.6 - height)/2);
+
     let response = document.getElementById("response");
-    let str = `Width: ${(parseFloat(right.value)-parseFloat(left.value))/200}<br>Height: ${(parseFloat(bottom.value)-parseFloat(top.value))/200}<br>X: ${(114.61249 - ((30480 - parseFloat(right.value))/200))}<br>Y: ${(79.13249 - ((20320 - parseFloat(bottom.value))/200))}`;
+    let str = `Width: ${width}<br>Height: ${height}<br>X: ${X}<br>Y: ${Y}`;
     response.innerHTML = str;
 });
